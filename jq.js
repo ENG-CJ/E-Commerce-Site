a013df9d020acdd54723dde1bf2379b5e5307e90
Abdulrahman-Haji <77126546+ENG-CJ@users.noreply.github.com>
2022-02-05 17:29:46 +0300

    initial commoit

---
+++ jq.js
@@ -0,0 +1,3 @@
+//$(document).ready(()=>{
+//  $('[data-toggle="popover"]').popover();
+//});
---
+++ js/app.js
@@ -0,0 +1,40 @@
+// js file
+
+// variables
+let cart=document.querySelector('.cart');
+let buyNow=document.querySelector('.buy-now');
+let watch=document.querySelector('.watch')
+
+
+//evennts
+cart.addEventListener('click',info);
+buyNow.addEventListener('click',buy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// functions
+function info(){
+    alert('This Is Not Currently Working\nWe Will Fix')
+}
+
+function buy(){
+    ans=prompt('Value');
+    if (ans==20){
+        alert('Buyed')
+    }else{
+        alert('Invalid Value')
+    }
+}
+
+
